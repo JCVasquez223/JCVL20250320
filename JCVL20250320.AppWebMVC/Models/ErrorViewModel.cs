61b695b7869c9d6f3b1bc7dd47d0148d97810cf4
Vasquez <vasquez0215@outlook.com>
2025-03-20 07:43:56 -0600

    Agregar archivos de proyecto.

---
+++ JCVL20250320.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JCVL20250320.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
